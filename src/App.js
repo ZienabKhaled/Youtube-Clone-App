ed43e5892a81038952e6fd9786da7ea869f00fd9
Zienab Khaled <zienabkhaled246@gmail.com>
2023-05-28 02:46:34 +0300

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes,Route } from 'react-router-dom';
 import { Box } from '@mui/material';
 
-import {Navbar , Feed , VideoDetail , ChannelDetail, SearchFeed , SearchBar} from './components';
+import {Navbar , Videos , Feed , VideoDetail , ChannelDetail, SearchFeed , SearchBar , Sidebar} from './components';
 
 const App = () => (
     <BrowserRouter>

eadfd1bad9fe5590e544d9713bf4211653c9c3b9
Zienab Khaled <zienabkhaled246@gmail.com>
2023-05-31 21:48:52 +0300

    finishing up

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,81 +1,126 @@
-import { useState, useEffect } from "react";
-import { Stack, Box , Typography } from "@mui/material";
-import Sidebar from "./Sidebar";
-import Videos from "./Videos";
+// import { useState, useEffect } from "react";
+// import { Stack, Box , Typography } from "@mui/material";
+// import Sidebar from "./Sidebar";
+// import Videos from "./Videos";
+
+// import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+// const Feed = () => {
+
+//   const [selectedCategory, setSelectedCategory] = useState('New');
+// const [videos, setVideos] = useState(null);  
+//   // Mostafa
+//   // const params = { part: "snippet", q: selectedCategory, maxResults: "50" };
+
+//   // useEffect(() => {
+//   //     fetchFromAPI(`search`, params)
+//   //     .then((data)=> setVideos(data.items)
+//   //     )
+//   // }, [params]);
+  
+
+//   //Original
+//   useEffect(() => {
+//     setVideos(null);
+
+//     fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+//       .then((data) => setVideos(data.items))
+//     }, [selectedCategory]);
+
+
+// // return(
+// //   <Stack sx={{ flexDirection:{
+// //     sx:"column" , md:"row"} 
+// //   }}>
+// //     <Box sx={{ height:{ sx: 'auto' , md: '92vh' } 
+// //   , borderRight: '1px solid #3d3d3d ' , px:{ sx: 0 , md: 2} }}>
+
+// //   <Sidebar
+// //     selectedCategory={selectedCategory}
+// //     setSelectedCategory={setSelectedCategory}
+  
+// //   />
+
+// //   <Typography className="copyright"
+// //   variant="body2" sx={{ mt:1.5 , color:'#fff' }}
+// //   >
+// //   Copyright 2023 React Project
+// //   </Typography>
+// //     </Box>
+
+// //     <Box p={2} sx={{ msOverflowY:'auto', height:'90vh', flex:2 }}>
+// //       <Typography variant="h4" fontWeight="bold" mb={2}
+// //       sx={{ color:'#fff'}}
+// //       >
+// //         {selectedCategory} <span style={{ color:'#F31503' }}>
+// //           videos
+// //         </span>
+// //       </Typography>
+
+// //       <Videos videos={videos }   />
+// //       </Box>  
+
+// //   </Stack>
+// // )
+// // }
+// return (
+//   <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+//     <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+//       <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+      
+//       <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+//         Copyright © 2022 JSM Media
+//       </Typography>
+//     </Box>
+
+//     <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+//       <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+//         {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
+//       </Typography>
+
+//       <Videos videos={videos} />
+//     </Box>
+//   </Stack>
+// );
+// };
+
+// export default Feed
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
 import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Videos, Sidebar } from "./";
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState(null);
 
-  const [selectedCategory, setSelectedCategory] = useState('New');
-  const [videos , setVideos]= useState([]);
-  // Mostafa
-  const params = { part: "snippet", q: selectedCategory, maxResults: "50" };
+  useEffect(() => {
+    setVideos(null);
 
-  // useEffect(() => {
-  //     fetchFromAPI(`search`, params)
-  //     .then((data)=> setVideos(data.items)
-  //     )
-  // }, [params]);
-  
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
+    }, [selectedCategory]);
 
-  //Original
-  // useEffect(()=> {
-  //   fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
-  // },[selectedCategory])
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+        
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+          Copyright © 2022 JSM Media
+        </Typography>
+      </Box>
 
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+          {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
+        </Typography>
 
-  //ChatGPT Code
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const data = await fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
-  //       // Process the fetched data or update state with the data here
-  //       console.log(data);
-  //     } catch (error) {
-  //       // Handle any error that occurred during the fetch
-  //       console.error(error);
-  //     }
-  //   };
-  
-  //   fetchData();
-  // }, [selectedCategory]);
-  
+        <Videos videos={videos} />
+      </Box>
+    </Stack>
+  );
+};
 
-return(
-  <Stack sx={{ flexDirection:{
-    sx:"column" , md:"row"} 
-  }}>
-    <Box sx={{ height:{ sx: 'auto' , md: '92vh' } 
-  , borderRight: '1px solid #3d3d3d ' , px:{ sx: 0 , md: 2} }}>
-
-  <Sidebar
-    selectedCategory={selectedCategory}
-    setSelectedCategory={setSelectedCategory}
-  
-  />
-
-  <Typography className="copyright"
-  variant="body2" sx={{ mt:1.5 , color:'#fff' }}
-  >
-  Copyright 2023 React Project
-  </Typography>
-    </Box>
-
-    <Box p={2} sx={{ msOverflowY:'auto', height:'90vh', flex:2 }}>
-      <Typography variant="h4" fontWeight="bold" mb={2}
-      sx={{ color:'#fff'}}
-      >
-        {selectedCategory} <span style={{ color:'#F31503' }}>
-          videos
-        </span>
-      </Typography>
-
-      <Videos videos={videos }   />
-      </Box>  
-
-  </Stack>
-)
-}
-
-export default Feed
+export default Feed;
--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,41 +1,37 @@
 import axios from 'axios';
-// const BASE_URL ='https://youtube-v31.p.rapidapi.com';
-// const options = {
-//     method: 'GET',
-//     url: BASE_URL,
-//     params: {
-//         maxResults: '50'
-//       },
-//       headers: {
-//         'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-//         'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-//       }
-//     };
-//     try {
-//       const response = await axios.request(options);
-//       console.log(response.data);
-//     } catch (error) {
-//       console.error(error);
-//     }
 
-// export const fetchFromAPI = async (url) => {
-//     const {data} =await axios.get(`${BASE_URL}/${url}` , options);
-//     return data;
-// }
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
-
-const BASE_URL = "https://youtube-v31.p.rapidapi.com";
-
-export const fetchFromAPI = async (endPoint, params) => {
-    const options = {
-        method: "GET",
-        params: params,
-        headers: {
-            "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
-            "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
-        },
-    };
-    const { data } = await axios.get(`${BASE_URL}/${endPoint}`, options);
-    return data;
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
 };
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
+
+
+
+// const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+// export const fetchFromAPI = async (endPoint, params) => {
+//     const options = {
+//         method: "GET",
+//         params: params,
+//         headers: {
+//             "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+//             "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+//         },
+//     };
+//     const { data } = await axios.get(`${BASE_URL}/${endPoint}`, options);
+//     return data;
+// };
 
